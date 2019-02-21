ff8a6b876f26c60fcb3c3fdd3f5cdae072cb5573
mingjun <40516784+mingjunlu@users.noreply.github.com>
2019-02-21 15:26:51 +0800

    Start from scratch

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from 'react'
+
+class App extends React.Component {
+    render() {
+        return (
+            <Fragment>
+                <h1>Your app is running</h1>
+                <p>Try to add something now!</p>
+            </Fragment>
+        )
+    }
+}
+
+export default App
