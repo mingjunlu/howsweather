705262cb7201aa8e9456bb1694aff051aa0c4bfc
mingjun <40516784+mingjunlu@users.noreply.github.com>
2019-03-18 13:53:18 +0800

    Fix an issue where the location doesn't change accordingly to URL typed by users

--- src/utils/withGeolocation.js
+++ src/utils/withGeolocation.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import nearestLocation from './nearestLocation'
+import * as locations from './locations.json'
 
 // 回傳加上地點資訊的 Component
 const withGeolocation = (Component, props) => {
-    const coords = props.location.state
-        ? props.location.state.coords
-        : '25.038062,121.564448'    // 預設顯示臺北市天氣
+    let coords
+    if (props.location.state) { coords = props.location.state.coords }
+    else if (props.location.pathname === '/') { coords = '25.038062,121.564448' }
+    else {
+        const matchedPath = locations.default.find(loc => loc.path === `/${props.match.params[0]}/`)
+        if (matchedPath) { coords = matchedPath.coords }
+    }
     const bestGuess = nearestLocation(coords)
     return <Component {...props} key={props.location.key} geolocation={bestGuess} />
 }
